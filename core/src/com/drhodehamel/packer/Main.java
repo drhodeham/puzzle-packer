f782aae728f6702da2df3493fcba3c606826cc90
drkered <drhodeham@gmail.com>
2017-09-15 20:52:54 -0700

    Initial commit

---
+++ core/src/com/drhodehamel/packer/Main.java
@@ -0,0 +1,33 @@
+package com.drhodehamel.packer;
+
+import com.badlogic.gdx.ApplicationListener;
+
+public class Main implements ApplicationListener {
+
+	@Override
+	public void create() {
+
+	}
+
+	@Override
+	public void dispose() {
+
+	}
+
+	@Override
+	public void render() {
+
+	}
+
+	@Override
+	public void resize(int width, int height) {
+	}
+
+	@Override
+	public void pause() {
+	}
+
+	@Override
+	public void resume() {
+	}
+}
